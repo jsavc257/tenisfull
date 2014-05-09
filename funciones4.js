334a1102f8195ea7b430110f7864afa65210d308
jsavc257 <jsavc80@gmail.com>
2014-05-09 15:33:55 -0500

    modificación 4
    modificación 4 de orientación

--- funciones4.js
+++ funciones4.js
@@ -1,7 +1,8 @@
 // JavaScript Document
 $(document).on("pagecreate",function(event){
   $(window).on("orientationchange",function(event){
-    alert("Orientation changed to: " + event.orientation);
+  //  alert("Orientation changed to: " + event.orientation);
+    alert("Cambie la posición de vertical a horizontal de su dispositivo para mejor uso");
   });                     
 });
 
@@ -9,11 +10,11 @@ $(document).on("pagecreate",function(event){
   $(window).on("orientationchange",function(){
     if(window.orientation == 0)
     {
-      $("#indica3").text("LOS MEJORES PRECIOS").css({"background-color":"yellow","font-size":"35%"});
+      $("#indica3").text("LOS MEJORES PRECIOS").css({"background-color":"yellow","font-size":"250%"});
     }
     else
     {
-      $("#indica4").text("GRACIAS POR TU VISITA").css({"background-color":"pink","font-size":"40%"});
+      $("#indica4").text("GRACIAS POR TU VISITA").css({"background-color":"pink","font-size":"200%"});
     }
   });                   
 });
